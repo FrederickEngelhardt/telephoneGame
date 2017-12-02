98ba85eea7684be60dfee2be20b4f65566d33c31
feng <feng@bizdev.us>
2017-12-02 13:02:10 -0700

    made the text function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
+// let status = 'textbox'
 let status = 'drawing'
-
+let count = 1
 function getStatus (status){
-  if (status === 'drawing') {return createTextbox}
-  else if (status === 'textbox') {return createDrawingBoard}
+  if (status === 'drawing') {return createTextbox()}
+  else if (status === 'textbox') {return createDrawingBoard()}
 }
-// const createTextbox = () => {
-//   $('addon').append()
-// }
-
-
-
+const createTextbox = () => {
+  count++
+  $('.addon').append(
+    `<div class='row'>
+        <div class="table${count}">
+          <input></input><a id="button${count}" class="waves-effect waves-light btn"><i class="materisal-icons right sendTxt"></i>button</a>
+        </div>
+      </div>`)
+  createListeners()
+}
+const createListeners = () => {
+  return $(`#button${count}`).click( () => {
+    getStatus(status)
+  })
+}
 $(document).ready( () => {
-
+  createListeners()
 })
